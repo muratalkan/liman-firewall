047dd6e01c4bd08405c9c4cf9d12c1cd770eec49
Murat Alkan <muratalkanw@hotmail.com>
2021-08-12 15:47:56 +0300

    Update InstallPackage.php

--- app/Tasks/InstallPackage.php
+++ app/Tasks/InstallPackage.php
@@ -14,25 +14,25 @@ class InstallPackage extends Task
 
 	public function __construct(array $attrbs=[]){
 
-		$packages = "ufw lsof";
+		$packages = ['ufw', 'lsof'];
 
-		$this->control = Distro::debian('apt-get install')
+		$this->control = Distro::debian('apt\|dpkg')
 			->centos('yum install')
 			->get();
 
 		$this->command = Distro::debian(
-			'DEBIAN_FRONTEND=noninteractive apt-get install '.$packages.' -y'
+			'DEBIAN_FRONTEND=noninteractive apt-get install '.implode(" ", $packages).' -y'
 		)
 			->centos(
 				'
 					yum install epel-release yum-utils -y;
-					yum install '.$packages.' -y
+					yum install '.implode(" ", $packages).' -y
 				'
 			)
 			->get();
 
 		$this->attributes = $attrbs;
-		$this->logFile = Formatter::run('/tmp/apt-install_initialPackages.txt');
+		$this->logFile = Formatter::run('/tmp/apt-installPackages.txt');
 	}
 
 }
